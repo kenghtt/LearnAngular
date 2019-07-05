c285f01141cbfefefa02304924a7642f7f056273
Jeremy Yang (jyang108) <jeremy.yang@optum.com>
2019-07-05 15:00:47 -0500

    Currently at: 2. Style the heroes

--- src/main/java/AngularStuff/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ src/main/java/AngularStuff/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -12,9 +14,14 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
+  heroes = HEROES;
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
